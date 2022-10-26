8d1f7cc194ab2bde9c15d524edacaceb7582c3b1
Gogs <gogs@fake.local>
2022-10-26 13:17:32 +0300

    VarVerViewSec

--- VWH/MainWindow.xaml.cs
+++ VWH/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace VWH
 {
@@ -22,7 +24,8 @@ namespace VWH
     public partial class MainWindow : Window
     {
         public static Entities Connection = new Entities();
-
+        DispatcherTimer Timer = new DispatcherTimer();
+        int i = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -31,6 +34,55 @@ namespace VWH
             DGDate.ItemsSource = Connection.StudPars.ToList();
             TxtName.Text = Connection.stud.First().Name;
             CmdDays.ItemsSource = Connection.DatePars.ToList();
+            Timer.Interval = new TimeSpan(1000000);
+            Timer.Tick += Timer_Tick;
+            
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            i++;
+            //MessageBox.Show($"{i}");
+            // ListData.Margin = new Thickness(i,i,i,i);
+            ProgressB.Opacity += 0.01;
+            ProgressB.Value = i;
+            switch (i)
+            {
+                case 30:
+                    {
+                        Timer.Stop();
+
+                        Timer.Start();
+                        break;
+                    }
+                case 50:
+                    {
+                        Timer.Stop();
+
+                        Timer.Start();
+                        break;
+                    }
+                case 70:
+                    {
+                        Timer.Stop();
+
+                        Timer.Start();
+                        break;
+                    }
+                case 100:
+                    {
+                        Timer.Stop();
+                        ProgressB.Value = 0;
+                        ProgressB.Opacity = 0;
+                        i = 0;
+                        MessageBox.Show("1000 Баллллов Грифффендору");
+                        Timer.Start();
+
+                        break;
+                    }
+            }
+           
         }
 
         private void CmdDays_Selected(object sender, RoutedEventArgs e)
